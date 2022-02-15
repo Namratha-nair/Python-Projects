0909fb3ec91378e0d72a3b99be33cd3efd322ab3
Namratha J Nair <namratha.nair1@gmail.com>
2022-02-15 18:58:41 +0530

    Add files via upload

---
+++ Multiple_Dice_Roller/Multiple_Dice_Roller.py
@@ -0,0 +1,31 @@
+import random
+
+print("Welcome to the Multiple Dice Roller")
+n = input("Enter the number of dice you want to roll: ")
+n = int(n)                       # convert it to integer datatype
+
+
+def roll_dice():                 # define a function to roll the dice
+    return random.randint(1, 6)  # produce a random number from 1 to 6
+
+
+output = []                      # create an empty list to store the output
+
+
+# initially set the user preference to roll the dice as yes
+option = "y"
+
+if n > 0:                        # check if the number of dice value is valid
+    while option == "y":
+        for i in range(n):       # to generate the output for all the dice
+            x = roll_dice()
+            output.append(x)
+        print("The output for " + str(n) + " dice:")
+        print(output)            # display the output
+        output = []              # reset the output to an empty list after every roll
+        print("\n")
+        option = input(
+            "Press y to roll again or press any other key to exit: ")
+        print("\n")
+else:
+    print("Enter a valid number of dice above 0")
