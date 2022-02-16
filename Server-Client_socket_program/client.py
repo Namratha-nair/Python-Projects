653347a7801e72de3d50243516b3694834cd64ef
Namratha J Nair <namratha.nair1@gmail.com>
2022-02-16 16:45:33 +0530

    Update client.py

--- Server-Client_socket_program/client.py
+++ Server-Client_socket_program/client.py
@@ -1,16 +1,15 @@
-import socket
+import socket                                               # Socket module provides socket operations and some related functions.
 
+host = socket.gethostname()                                 #get the host name
+port = 5555                                                 #initaiate to any port number above 1024 
 
-host = socket.gethostname()
-port = 5555
+mysocket = socket.socket()                                  #create an instance
+mysocket.connect((host, port))                              #connect to the server
 
-mysocket = socket.socket()
-mysocket.connect((host, port))
-
-s_message = input("> ")
+s_message = input("> ")                                     #take input
 while s_message.lower().strip() != "bye":
-    mysocket.send(s_message.encode())
-    r_message = mysocket.recv(1024).decode()
-    print("Received message: " + str(r_message))
-    s_message = input("> ")
-mysocket.close()
+    mysocket.send(s_message.encode())                       #send data
+    r_message = mysocket.recv(1024).decode()                #receive data
+    print("Received message: " + str(r_message))            #display the message in the terminal
+    s_message = input("> ")                                 #take input again
+mysocket.close()                                            #close connection
