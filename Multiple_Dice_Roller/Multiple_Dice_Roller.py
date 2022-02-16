85814b3a17c12ee8758f8c959f70b932b7d67325
Namratha J Nair <namratha.nair1@gmail.com>
2022-02-16 09:49:02 +0530

    Update Multiple_Dice_Roller.py

--- Multiple_Dice_Roller/Multiple_Dice_Roller.py
+++ Multiple_Dice_Roller/Multiple_Dice_Roller.py
@@ -1,8 +1,14 @@
 import random                    #use the random module to generate a random number
 
-print("Welcome to the Multiple Dice Roller")
-n = input("Enter the number of dice you want to roll: ")
-n = int(n)                       # convert it to integer datatype
+def get_input():                 #define a function to get and validate the input
+    print("Welcome to the Multiple Dice Roller")
+    n = input("Enter the number of dice you want to roll: ")
+    n = int(n)                       # convert the input to integer datatype
+    if n <= 0:                       # check if the number of dice value is valid
+        print("Error: Try a valid dice number above 0\n")
+        get_input()
+    else:
+        return n
 
 
 def roll_dice():                 # define a function to roll the dice
@@ -14,18 +20,15 @@ output = []                      # create an empty list to store the output
 
 
 option = "y"                     # initially set the user preference to roll the dice as yes
+n = get_input()
 
-if n > 0:                        # check if the number of dice value is valid
-    while option == "y":
-        for i in range(n):       # to generate the output for all the dice
-            x = roll_dice()
-            output.append(x)
-        print("The output for " + str(n) + " dice:")
-        print(output)            # display the output
-        output = []              # reset the output to an empty list after every roll
-        print("\n")
-        option = input(
-            "Press y to roll again or press any other key to exit: ")
-        print("\n")
-else:
-    print("Error: Try entering a valid dice number above 0")
+while option == "y":
+    for i in range(n):            # to generate the output for all the dice
+        x = roll_dice()
+        output.append(x)
+    print("The output for " + str(n) + " dice:")
+    print(output)                 # display the output
+    output = []                   # reset the output to an empty list after every roll
+    print("\n") 
+    option = input("Enter y to roll again or enter any other key to exit: ")
+    print("\n")
