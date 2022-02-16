b158b108f08d6e7bbafa7e0165966664a37f4795
Namratha J Nair <namratha.nair1@gmail.com>
2022-02-16 16:39:37 +0530

    Update server.py

--- Server-Client_socket_program/server.py
+++ Server-Client_socket_program/server.py
@@ -1,20 +1,20 @@
-import socket
+import socket                                           # Socket module provides socket operations and some related functions.
 
 
-host = socket.gethostname()
-port = 5555
+host = socket.gethostname()                             #get the host name
+port = 5555                                             #initiate any port above 1024
 
-mysocket = socket.socket()
-mysocket.bind((host, port))
+mysocket = socket.socket()                              #create an instance
+mysocket.bind((host, port))                             #bind host and port together
 
-mysocket.listen(5)
-conn, address = mysocket.accept()
+mysocket.listen(5)                                      #configure the server to listen to 5 clients simultaneously
+conn, address = mysocket.accept()                       #accept new connection
 print("Connection from: " + str(address))
-while True:
-    r_message = conn.recv(1024).decode()
-    if not r_message:
+while True:                                             #provide a forever loop until an error occurs
+    r_message = conn.recv(1024).decode()                #data more than 1024 bytes won't be accepted
+    if not r_message:                                   #if data is not received, berak
         break
-    print("Message received: " + str(r_message))
+    print("Message received: " + str(r_message))        #display the received message
     s_message = input("> ")
-    conn.send(s_message.encode())
-conn.close()
+    conn.send(s_message.encode())                       #send data to the client
+conn.close()                                            #close the connection
