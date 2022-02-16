2b1117cf24f5f12227c86e3f3d5800c7ff0a3173
Namratha J Nair <namratha.nair1@gmail.com>
2022-02-16 10:24:11 +0530

    Update and rename IP_address_generator/IP_address_generator.py to IP_address_finder/IP_address_finder.py

--- IP_address_finder/IP_address_finder.py
+++ IP_address_finder/IP_address_finder.py
@@ -2,12 +2,15 @@ import socket                                           # Socket module provides
 
 
 def get_IP():                                           #define a function to get the IP address
-    hname = input("Enter the website address(URL):")    #take the host name - url as input
+    host = input("Enter the website address(URL):")     #take the host name - url as input
     try:                                                #test a for errors
-        print(f"Hostname: {hname}")                     #print the host name
-        print(f"IP: {socket.gethostbyname(hname)}")     #print the IP address of the host
+        print(f"Hostname: {host}")                      #print the host name
+        print(f"IP: {socket.gethostbyname(host)}")      #print the IP address of the host
     except socket.gaierror as error:                    #store the getaddrinfo error thrown, in the variable, error
         print(f"Invalid Hostname! Error is {error}")    #print the error
 
 
-get_IP()
+choice = "y"                                             #set choice to y initially
+while choice == "y":
+    get_IP()
+    choice = input("\nEnter y to continue finding IP address or Press any key to exit!: ")
